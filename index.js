91cde8acbd0c2cf88582769d5f5037987e8ad95a
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-11-09 00:06:46 +0300

    Added load secondary images function

--- index.js
+++ index.js
@@ -13,16 +13,44 @@ function loadImages() {
       let photosArray = data.photos;
       console.log(photosArray);
 
-      const cardSvgElements = document.querySelectorAll(".card-img-top");
+      const cardImgElements = document.querySelectorAll(".card-img-top");
 
-      console.log("card svg elements: ", cardSvgElements);
-      for (let i = 0; i < cardSvgElements.length; i++) {
-        const cardImgElement = document.createElement("img");
-        cardImgElement.classList.add = "card-img-top w-100";
-        cardImgElement.style.height = "15vw";
-        cardImgElement.style.objectFit = "cover";
-        cardImgElement.src = `${photosArray[i].src.medium}`;
-        cardSvgElements[i].replaceWith(cardImgElement);
+      console.log("card svg elements: ", cardImgElements);
+      for (let i = 0; i < cardImgElements.length; i++) {
+        const newCardImgElement = document.createElement("img");
+        cardImgElements[i].replaceWith(newCardImgElement);
+        newCardImgElement.classList = "card-img-top w-100";
+        newCardImgElement.style.height = "15vw";
+        newCardImgElement.style.objectFit = "cover";
+        newCardImgElement.src = `${photosArray[i].src.medium}`;
       }
     });
 }
+
+const loadSecondaryImages = function () {
+  fetch("https://api.pexels.com/v1/search?query=book", {
+    headers: {
+      Authorization: "563492ad6f91700001000001e4fe95e220c744558e9a894e99134392",
+    },
+  })
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      let photosArray = data.photos;
+      console.log("secondaryyyy ");
+
+      const cardImgElements = document.querySelectorAll(".card-img-top");
+
+      for (let i = 0; i < cardImgElements.length; i++) {
+        const newCardImgElement = document.createElement("img");
+        cardImgElements[i].replaceWith(newCardImgElement);
+        newCardImgElement.classList = "card-img-top w-100";
+        newCardImgElement.style.height = "15vw";
+        newCardImgElement.style.objectFit = "cover";
+        newCardImgElement.src = `${photosArray[i].src.original}`;
+      }
+    });
+};
