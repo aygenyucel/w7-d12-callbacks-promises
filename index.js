1e6c3195d53d626aef3f28d545ca904f49330485
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-11-08 23:43:44 +0300

    fixed fetching data and load images button

--- index.js
+++ index.js
@@ -1,44 +1,28 @@
-// import { createClient } from "pexels";
-
-// const client = createClient(
-//   "563492ad6f91700001000001e4fe95e220c744558e9a894e99134392"
-// );
-
-// fetch("https://api.pexels.com/v1/search", options)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
-
-fetch("https://api.pexels.com/v1/search?query=people", {
-  headers: {
-    Authorization: "563492ad6f91700001000001e4fe95e220c744558e9a894e99134392",
-  },
-})
-  .then((resp) => {
-    return resp.json();
+function loadImages() {
+  fetch("https://api.pexels.com/v1/search?query=people", {
+    headers: {
+      Authorization: "563492ad6f91700001000001e4fe95e220c744558e9a894e99134392",
+    },
   })
-  .then((data) => {
-    console.log(data);
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      console.log(data);
 
-    let photosArray = data.photos;
-    console.log(photosArray);
+      let photosArray = data.photos;
+      console.log(photosArray);
 
-    const cardElements = document.querySelectorAll(".card");
-    console.log(cardElements);
-    for (let i = 0; i < cardElements.length; i++) {
-      console.log(photosArray[i].url);
-      cardElements[
-        i
-      ].innerHTML += `<img class="card-img-top" src="${photosArray[i].url}" alt="">`;
-    }
-  });
+      const cardSvgElements = document.querySelectorAll(".card-img-top");
 
-// const loadImages = function () {
-//   const cardImgElements = document.querySelectorAll(".card-img-top");
-//   let i = 0;
-//   for (cardImg of cardImgElements) {
-//     cardImg.src = `${photosArray[i][url]}`;
-//     i++;
-//   }
-// };
+      console.log("card svg elements: ", cardSvgElements);
+      for (let i = 0; i < cardSvgElements.length; i++) {
+        const cardImgElement = document.createElement("img");
+        cardImgElement.classList.add = "card-img-top w-100";
+        cardImgElement.style.height = "15vw";
+        cardImgElement.style.objectFit = "cover";
+        cardImgElement.src = `${photosArray[i].src.medium}`;
+        cardSvgElements[i].replaceWith(cardImgElement);
+      }
+    });
+}
